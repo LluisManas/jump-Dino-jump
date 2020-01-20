19a08bfb15148e5ace06fd9ce5920d2cf32fe456
Lluís Mañas <manastorra@gmail.com>
2020-01-20 11:10:49 +0100

    message

---
+++ main.js
@@ -0,0 +1,23 @@
+function setup() {
+  createCanvas(600, 400);
+}
+
+function draw() {
+  background(220);
+}
+
+class Background {
+  constructor() {}
+}
+
+class Game {
+  constructor() {}
+}
+
+class Player {
+  constructor() {}
+}
+
+class Obstacle {
+  constructor() {}
+}
